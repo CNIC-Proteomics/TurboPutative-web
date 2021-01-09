c9e313c389426ce3d2c27b560a8ec3d6d9abb56d
Rafael Barrero Rodriguez <fitael@gmail.com>
2021-01-09 14:59:06 +0100

    Deploy app

---
+++ src/index.js
@@ -0,0 +1,25 @@
+// Import modules
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+
+// Global variables
+var app = express();
+
+// Settings
+app.set('port', process.env.PORT || 8080);
+
+// Middlewares
+app.use(morgan('combined'));
+
+// Routes
+app.use(require(path.join(__dirname, "routes/index.js")));
+app.use(require(path.join(__dirname, "routes/execute.js")));
+
+// Static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Start listening
+app.listen(app.get('port'), () => {
+    console.log('TurboPutative web application listening on port', app.get('port'));
+})
