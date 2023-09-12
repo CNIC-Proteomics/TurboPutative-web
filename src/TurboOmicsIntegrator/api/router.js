81f30b6be502c9e51f3e3a8bd1ae4f24dfa93b79
rbarreror <fitael@gmail.com>
2023-09-12 13:30:30 +0200

    Provide sample data

--- src/TurboOmicsIntegrator/api/router.js
+++ src/TurboOmicsIntegrator/api/router.js
@@ -1,7 +1,7 @@
 // Import modules
 const express = require('express');
 const fs = require('fs');
-const path=require('path');
+const path = require('path');
 
 // Variables
 const router = express.Router();
@@ -25,18 +25,44 @@ router.get('/search/:jobID', (req, res) => {
         // Attempt to access the file
         fs.accessSync(jobPath, fs.constants.F_OK);
         console.log(`Job found: ${req.params.jobID}`);
-    
+
         // Read JSON file with results and send to client
         const results = JSON.parse(fs.readFileSync(jobPath, 'utf-8'));
         console.log(`Results read: ${req.params.jobID}`);
 
-        res.json({exist: true, results: results});
-    
+        res.json({ exist: true, results: results });
+
     } catch (error) {
         console.log(`Error in search: ${req.params.jobID}`);
         console.log(error);
-        res.json({exist: false}); // File does not exist or cannot be accessed
+        res.json({ exist: false }); // File does not exist or cannot be accessed
     }
+})
+
+
+/*
+Descripción: Ruta para adquirir tablas de ejemplos desde cliente
+Params:
+    - No recibe ningún parámetro
+Return:
+    - Devuelve un objeto JSON con las 5 tablas necesarias para iniciar la creación del proyecto
+
+    TODO: Lo cambiaremos más adelante para cargar un trabajo
+*/
+router.get('/load_sample_data', (req, res) => {
+    console.log('Sending Sample Data');
+
+    const myPath = path.join(__dirname, '../misc/');
+
+    resJson = {
+        'xq': JSON.parse(fs.readFileSync(path.join(myPath, 'Xq_minus_X.json'), 'utf-8')),
+        'xm': JSON.parse(fs.readFileSync(path.join(myPath, 'Xm.json'), 'utf-8')),
+        'mdata': JSON.parse(fs.readFileSync(path.join(myPath, 'main_metadata.json'), 'utf-8')),
+        'q2i': JSON.parse(fs.readFileSync(path.join(myPath, 'q2info.json'), 'utf-8')),
+        'm2i': JSON.parse(fs.readFileSync(path.join(myPath, 'f2i.json'), 'utf-8'))
+    }
+
+    res.json(resJson);
 })
 
 module.exports = router;
