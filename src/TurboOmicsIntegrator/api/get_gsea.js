d1fd1ef92c7f2a9ef7489cf58153cfc720a2ba44
rbarreror <fitael@gmail.com>
2024-05-07 18:09:48 +0200

    finished mummichog

--- src/TurboOmicsIntegrator/api/get_gsea.js
+++ src/TurboOmicsIntegrator/api/get_gsea.js
@@ -16,42 +16,52 @@ router.get('/get_gsea/:jobID/:omic/:gseaID/:db', async (req, res) => {
     console.log(`${jobID}: Fetching GSEA data ${omic} --> ${gseaID} - ${db}`);
 
     // set paths
-    const myPath = path.join(__dirname, '../jobs', jobID, 'GSEA', omic, gseaID);
+    let myPath = path.join(__dirname, '../jobs', jobID, 'GSEA', omic, gseaID);
+    myPath = omic == 'm' ? path.join(myPath, db) : myPath;
+
+
+    // if error.log file exists, there was an error
+    const err = await new Promise(resolve => fs.readFile(
+        path.join(myPath, 'error.log'), 'utf-8',
+        (err, data) => err ? resolve(false) : resolve(true)
+    ));
+
+    if (err) {
+        console.log(`${jobID}: Error obtaining GSEA data ${omic} --> ${gseaID} - ${db}`);
+        res.json({ status: 'error', gseaRes: null });
+        return
+    }
 
     // Read file
+    // in mummichog, each result is in a different folder
+    let myPathResults = omic == 'm' ? fs.readdirSync(myPath).filter(e => e.includes('results'))[0] : '';
+    myPathResults = myPathResults == undefined ? '' : myPathResults;
+    myPathResults = path.join(myPath, myPathResults);
+
+    const myPathFileRes = omic == 'm' ?
+        path.join(myPathResults, 'tables/mcg_pathwayanalysis_results.tsv') :
+        path.join(myPathResults, `${db}_GSEA.json`);
+
     const gseaData = await new Promise(resolve => fs.readFile(
-        path.join(myPath, `${db}_GSEA.json`), 'utf-8',
+        myPathFileRes, 'utf-8',
         (err, data) => {
             if (err) {
                 resolve(false);
             } else {
-                resolve(JSON.parse(data));
+                resolve(omic == 'm' ? data : JSON.parse(data));
             }
         }
     ));
 
     // if file does not exist...
     if (!gseaData) {
-
-        // if error.log file exists, there was an error
-        const err = await new Promise(resolve => fs.readFile(
-            path.join(myPath, 'error.log'), 'utf-8',
-            (err, data) => err ? resolve(false) : resolve(true)
-        ));
-
-        if (err) {
-            console.log(`${jobID}: Error obtaining GSEA data ${omic} --> ${gseaID} - ${db}`);
-            res.json({ status: 'error', gseaRes: null});
-        } else {
-            res.json({status: 'waiting'});
-        }
-
-        return;
+        res.json({ status: 'waiting' });
+    } else {
+        console.log(`${jobID}: Sending GSEA data ${omic} --> ${gseaID} - ${db}`);
+        res.json({ status: 'ok', gseaRes: gseaData });
     }
 
-    console.log(`${jobID}: Sending GSEA data ${omic} --> ${gseaID} - ${db}`);
-    res.json({ status: 'ok', gseaRes: gseaData });
-
+    return;
 });
 
 // Export
--- src/TurboOmicsIntegrator/api/run_gsea.js
+++ src/TurboOmicsIntegrator/api/run_gsea.js
@@ -70,7 +70,7 @@ router.post('/run_gsea/:jobID/:omic/:gseaID/:os', async (req, res) => {
             console.log(`GSEA executed successfully on ${omic}`);
         } else {
             fs.appendFileSync(
-                path.join(myPath, `error.log`), JSON.stringify({status: 'error'})
+                path.join(myPath, `error.log`), JSON.stringify({ status: 'error' })
             );
             console.log(`GSEA Error on ${omic}`);
         }
@@ -80,14 +80,86 @@ router.post('/run_gsea/:jobID/:omic/:gseaID/:os', async (req, res) => {
 
 });
 
-router.post('/run_mummichog/:jobID/:omic/:gseaID/:os', (req, res) => {
+router.post('/run_mummichog/:jobID/:omic/:gseaID/:os', async (req, res) => {
 
-    const { jobID, gseaID } = req.params;
+    const { jobID, omic, gseaID, os } = req.params;
     const gseaData = req.body;
 
+    // Set working path
+    const myPath = path.join(__dirname, '../jobs', jobID, 'GSEA', omic, gseaID);
 
-    console.log(gseaData);
-    console.log(jobID, gseaID);
+    // Check if gseaID exists
+    const existGseaId = await new Promise(resolve => {
+        fs.access(myPath, (err) => { resolve(!err) })
+    });
+
+    // If exists send it
+    /*if (existGseaId) {
+        res.json({ status: 'exists' });
+        return;
+    }*/
+
+    // Write working path
+    await new Promise(resolve => {
+        fs.mkdir(myPath, {}, () => resolve(0))
+    });
+
+    // 
+    const myPromises = Object.keys(gseaData).map(mode => {
+        return new Promise(async resolve => {
+
+            // set path for mode
+            const myPathMode = path.join(myPath, mode);
+
+            // create mode folder
+            await new Promise(r => fs.mkdir(myPathMode, () => r(0)));
+
+            // write infile.tsv
+            const infilePath = path.join(myPathMode, 'infile.tsv');
+            await new Promise(r => fs.writeFile(infilePath, gseaData[mode], () => r(0)));
+
+            // Execute mummichog
+            const exec = [
+                '-m', 'mummichog.main',
+                `--infile=${infilePath}`,
+                `--output=results`,
+                `--workdir=${myPathMode}`,
+                `--mode=${ mode == 'pos' ? 'positive' : 'negative'}`,
+                `--permutation=100`,
+                `-c`, `0.1`
+            ];
+
+            const process = spawn(
+                global.pythonPath,
+                exec,
+                { encoding: 'utf-8' }
+            );
+
+            process.stdout.on('data', data => fs.appendFileSync(
+                path.join(myPathMode, `.log`), `stdout: ${data}`)
+            );
+
+            process.stderr.on('data', data => fs.appendFileSync(
+                path.join(myPathMode, `.log`), `stderr: ${data}`)
+            );
+
+            process.on('close', code => {
+                if (code == 0) {
+                    console.log(`mummichog executed successfully on ${omic}`);
+                } else {
+                    fs.appendFileSync(
+                        path.join(myPathMode, `error.log`), JSON.stringify({ status: 'error' })
+                    );
+                    console.log(`mummichog Error on ${omic}`);
+                }
+            });
+
+            resolve(0);
+
+        });
+    });
+
+    await new Promise(r => Promise.all(myPromises).then((values) => r(0)));
 
     res.json({ status: 'running' });
 
