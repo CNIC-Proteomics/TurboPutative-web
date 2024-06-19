3fadfa71ff020ef17fa6ebeff8a57a768d2c5939
rbarreror <fitael@gmail.com>
2024-06-19 18:30:56 +0200

    lets go

--- src/TurboOmicsIntegrator/api/router.js
+++ src/TurboOmicsIntegrator/api/router.js
@@ -18,7 +18,6 @@ router.use('/', require('./run_turboputative'));
 router.use('/', require('./get_turboputative'));
 router.use('/', require('./run_gsea'));
 router.use('/', require('./get_gsea'));
-//router.use('/', require('./get_cmm.js'));
 
 
 // Export
