b89d1e8e68bf9ba4d833b705f177b1e6b5e351f8
rbarreror <fitae@DESKTOP-IJMB315>
2024-09-09 18:02:03 +0200

    add R dependencies

--- install-R-dependencies.R
+++ install-R-dependencies.R
@@ -1,26 +1,29 @@
 # Install R dependencies
 
-cat("** Install BiocManager")
-install.packages("BiocManager")
+cat("** Install BiocManager\n")
+install.packages("BiocManager", repos="https://cloud.r-project.org")
 
-cat("** Install jsonlite")
-install.packages("jsonlite")
+cat("** Install jsonlite\n")
+install.packages("jsonlite", repos="https://cloud.r-project.org")
 
-cat("** Install utf8")
+install.packages("https://cran.r-project.org/src/contrib/Archive/Matrix/Matrix_1.6-5.tar.gz", repos=NULL)
+install.packages("https://cran.r-project.org/src/contrib/Archive/MASS/MASS_7.3-60.0.1.tar.gz", repos=NULL)
+
+cat("** Install utf8\n")
 install.packages("https://cran.r-project.org/src/contrib/utf8_1.2.4.tar.gz", repos=NULL)
 
-cat("** Install fgsea")
-BiocManager::install("fgsea", , update = FALSE)
+cat("** Install fgsea\n")
+BiocManager::install("fgsea", update = FALSE)
 
-cat("** Install reactome.db")
-BiocManager::install("reactome.db", , update = FALSE)
+cat("** Install reactome.db\n")
+BiocManager::install("reactome.db", update = FALSE)
 
-cat("** Install vsn")
-BiocManager::install("vsn", , update = FALSE)
+cat("** Install vsn\n")
+BiocManager::install("vsn", update = FALSE)
 install.packages("https://cran.r-project.org/src/contrib/hexbin_1.28.4.tar.gz", repos=NULL)
 
-cat("** Install Biobase")
-BiocManager::install("Biobase", , update = FALSE)
+cat("** Install Biobase\n")
+BiocManager::install("Biobase", update = FALSE)
 
-cat("** Install MultiAssayExperiment")
+cat("** Install MultiAssayExperiment\n")
 BiocManager::install("MultiAssayExperiment", update = FALSE)
